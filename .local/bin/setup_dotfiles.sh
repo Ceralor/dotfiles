95b1fc21bb3c0a817d64f54405943f276b87e0b9
Kay Ohtie <kay@coyotesin.space>
2022-11-23 18:24:46 -0600

    Update setup_dotfiles.sh

--- .local/bin/setup_dotfiles.sh
+++ .local/bin/setup_dotfiles.sh
@@ -15,9 +15,9 @@ homegit config status.showUntrackedFiles no
 find ~/.local/bin/ -name "*.sh" -type f -exec chmod +x {} \;
 source ~/.bash_profile
 echo "Setup additional bash config? "
-select yn in "Yes" "No"; do
+select yn in y n; do
 	case $yn in
-  	Yes ) ~/.local/bin/setup_bashconfigs.sh; break;;
-  	No ) exit;;
+  	y ) ~/.local/bin/setup_bashconfigs.sh; break;;
+  	n ) exit;;
   esac
 done
